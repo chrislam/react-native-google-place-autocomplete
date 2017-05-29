f9b03b7e3302c9de05f78a921400e541d37a2239
Chris Lam <chris.lam@gladeye.co.nz>
2017-05-29 18:12:18 +1200

    Add text input

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {
+    StyleSheet,
+    TextInput,
     View
 } from 'react-native';
 
@@ -9,8 +11,37 @@ class GooglePlaceAutocomplete extends Component {
     }
 
     render() {
-        return <View></View>;
+        return (
+            <View style={style.container}>
+                <View style={style.inputContainer}>
+                    <TextInput style={style.input} />
+                </View>
+            </View>
+        );
     }
 }
+
+export const style = StyleSheet.create({
+    container: {},
+
+    inputContainer: {
+        backgroundColor: '#C9C9CE',
+        height: 44,
+    },
+
+    input: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 5,
+        fontSize: 15,
+        height: 28,
+        marginLeft: 8,
+        marginRight: 8,
+        marginTop: 7.5,
+        paddingBottom: 4.5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 4.5,
+    }
+});
 
 export default GooglePlaceAutocomplete;
