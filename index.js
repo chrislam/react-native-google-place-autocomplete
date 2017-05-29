d5182174e409e15e0f18fff00fa4f1da6c5bc014
Chris Lam <chris.lam@gladeye.co.nz>
2017-05-29 18:26:02 +1200

    Move input into own component

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import {
     StyleSheet,
-    TextInput,
     View
 } from 'react-native';
+import AutocompleteInput from './lib/AutocompleteInput';
 
 class GooglePlaceAutocomplete extends Component {
     constructor(props) {
@@ -13,35 +13,14 @@ class GooglePlaceAutocomplete extends Component {
     render() {
         return (
             <View style={style.container}>
-                <View style={style.inputContainer}>
-                    <TextInput style={style.input} />
-                </View>
+                <AutocompleteInput />
             </View>
         );
     }
 }
 
 export const style = StyleSheet.create({
-    container: {},
-
-    inputContainer: {
-        backgroundColor: '#C9C9CE',
-        height: 44,
-    },
-
-    input: {
-        backgroundColor: '#FFFFFF',
-        borderRadius: 5,
-        fontSize: 15,
-        height: 28,
-        marginLeft: 8,
-        marginRight: 8,
-        marginTop: 7.5,
-        paddingBottom: 4.5,
-        paddingLeft: 10,
-        paddingRight: 10,
-        paddingTop: 4.5,
-    }
+    container: {}
 });
 
 export default GooglePlaceAutocomplete;
