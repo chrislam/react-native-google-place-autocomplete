cff9698f69f154831c0aba02cbc24f7d550feec0
Chris Lam <chris.lam@gladeye.co.nz>
2017-05-29 14:55:13 +1200

    Initial commit 🚀

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Native Google Place Autocomplete
+
+React Native components for the Place Autocomplete service of the Google Places API Web Service
---
+++ index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import AutocompleteInput from './lib/AutocompleteInput';
+import Predictions from './lib/Predictions';
+import Prediction from './lib/Prediction';
+
+class GooglePlaceAutocomplete extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return null;
+    }
+}
+
+export AutocompleteInput;
+export Predictions;
+export Prediction;
+
+export default GooglePlaceAutocomplete;
