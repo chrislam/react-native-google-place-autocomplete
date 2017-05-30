74b82993aaedfb54f22ef820c86bdd870b84e0d4
Chris Lam <chris.lam@gladeye.co.nz>
2017-05-30 15:17:28 +1200

    Connect to Google Place Details and Place Autocomplete API's

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {
     StyleSheet,
     View
 } from 'react-native';
+import axios, { CancelToken } from 'axios';
 import AutocompleteInput from './lib/AutocompleteInput';
 import Predictions from './lib/Predictions';
 
@@ -11,12 +12,16 @@ class GooglePlaceAutocomplete extends Component {
         debounce: 250
     }
 
+    _cancelTokenSource;
+
     constructor(props) {
         super(props);
 
         this.state = {
             value: ''
         };
+
+        this._cancelTokenSource = CancelToken.source();
     }
 
     componentDidMount() {
@@ -49,19 +54,98 @@ class GooglePlaceAutocomplete extends Component {
     }
 
     _handleChangeText = (value) => {
-        this.props.onChangeText(value);
-        this.setState({
-            value
-        });
+        this.setState({value});
+
+        // Fire event
+        if (this.props.onChangeText) {
+            this.props.onChangeText(value);
+        }
     }
 
     _handleChangeTextSettle = () => {
-        // Get predictions
-        this.props.onPredictions();
+        if (this.state.value.length > 0) {
+            // Get predictions
+            this._request('/place/autocomplete', {
+                input: this.state.value
+            })
+            .then(response => {
+                if (response && response.data) {
+                    switch (response.data.status) {
+                        case 'OK':
+                        case 'ZERO_RESULTS':
+                            this._predictions(response.data.predictions);
+                            break;
+
+                        default:
+                            console.error('Request Error:', response.data.error_message || response.data.status);
+                            break;
+                    }
+                }
+            })
+            .catch(error => {
+                console.log('error', error);
+            });
+        } else {
+            if (this.state.predictions && this.state.predictions.length > 0) {
+                this._predictions(null);
+            }
+        }
+    }
+
+    _predictions(predictions) {
+        this.setState({predictions});
+
+        // Fire event
+        if (this.props.onPredictions) {
+            this.props.onPredictions(predictions);
+        }
     }
 
     _handlePressPrediction = (prediction) => {
-        this.props.onPress(prediction);
+        // Get more detail about the place
+        this._request('/place/details', {
+            placeid: prediction.place_id
+        })
+        .then(response => {
+            if (response && response.data) {
+                if (response.data.status === 'OK') {
+                    const {result} = response.data;
+
+                    // Fire event
+                    if (this.props.onResult) {
+                        this.props.onResult(result);
+                    }
+                } else {
+                    console.error('Request Error:', response.data.error_message || response.data.status);
+                }
+            }
+        })
+        .catch(error => {
+            console.log('error', error);
+        });
+    }
+
+    _request = (url, params) => {
+        // Cancel any other requests
+        this._cancelTokenSource.cancel('The "clean slate" protocol');
+        this._cancelTokenSource = CancelToken.source();
+
+        params.key = this.props.googleAPIKey;
+
+        return axios({
+            url: `${url}/json`,
+            method: 'get',
+            baseURL: 'https://maps.googleapis.com/maps/api',
+            params,
+            cancelToken: this._cancelTokenSource.token
+        })
+        .catch(error => {
+            if (axios.isCancel(error)) {
+                console.log('Request Cancel:', error.message);
+            } else {
+                throw error;
+            }
+        });
     }
 }
 
