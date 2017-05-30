d058e854df829ac1aeab4f0d30ac8486d97cd7d9
Chris Lam <chris.lam@gladeye.co.nz>
2017-05-30 13:18:30 +1200

    Set up state for the query value

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ import Predictions from './lib/Predictions';
 class GooglePlaceAutocomplete extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            value: ''
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            value: this.props.value
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.value !== this.props.value) {
+            this.setState({
+                value: nextProps.value
+            });
+        }
     }
 
     render() {
